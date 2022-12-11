38aae63f305bc830668fa25379542ad810a06737
Blanca Ubis <ubis.blanca@gmail.com>
2022-12-11 18:50:18 +0100

    Change title

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,15 @@
 import logoAdalab from '../images/logo-adalab.png';
-import './../styles/layout/Footer.scss'
+import './../styles/layout/Footer.scss';
 const Footer = () => {
-
-    return (
-<footer className="footer">
-        <p className="footer__text">Awesome profile-cards @2022</p>
-        <div className="footer__box">
-          <img src={logoAdalab} alt="Adalab" className="footer__box--logo" />
-        </div>
-      </footer>
-
-    )
+  return (
+    <footer className="footer">
+      <p className="footer__text">
+        Las Duquesas - Aristocratic profile-cards @2022
+      </p>
+      <div className="footer__box">
+        <img src={logoAdalab} alt="Adalab" className="footer__box--logo" />
+      </div>
+    </footer>
+  );
 };
 export default Footer;
